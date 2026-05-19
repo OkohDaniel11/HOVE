8337814813d2fe70b415e8df634cd7814c2445c7
OkohDaniel11 <129377762+OkohDaniel11@users.noreply.github.com>
2026-05-19 12:33:26 +0000

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,82 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     // If they have visited before, the class is ignored and the line displays instantly
 });
+
+// GSAP Scroll Trigger Function
+gsap.registerPlugin(ScrollTrigger);
+
+// Example 1: Fade in elements on scroll
+function scrollFadeIn() {
+    gsap.utils.toArray(".fade-in-scroll").forEach((element) => {
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top 80%",
+                end: "top 50%",
+                scrub: 1,
+                markers: false,
+            },
+            opacity: 1,
+            duration: 1,
+        });
+    });
+}
+
+// Example 2: Slide in from left on scroll
+function scrollSlideInLeft() {
+    gsap.utils.toArray(".slide-left-scroll").forEach((element) => {
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top 75%",
+                end: "top 25%",
+                scrub: 1,
+            },
+            x: 0,
+            opacity: 1,
+            duration: 0.8,
+        });
+    });
+}
+
+// Example 3: Scale up on scroll
+function scrollScaleUp() {
+    gsap.utils.toArray(".scale-scroll").forEach((element) => {
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top center",
+                end: "center center",
+                scrub: 2,
+            },
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+        });
+    });
+}
+
+// Example 4: Rotate on scroll
+function scrollRotate() {
+    gsap.utils.toArray(".rotate-scroll").forEach((element) => {
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top 80%",
+                end: "bottom 50%",
+                scrub: 1.5,
+            },
+            rotation: 360,
+            opacity: 1,
+            duration: 1,
+        });
+    });
+}
+
+// Call all scroll functions on page load
+document.addEventListener("DOMContentLoaded", () => {
+    scrollFadeIn();
+    scrollSlideInLeft();
+    scrollScaleUp();
+    scrollRotate();
+});
